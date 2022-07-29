97d8bec112ba79fc44c4552f9e1be8c8448de6a0
adamgulamhusein <adam.gulamhusein@ucalgary.ca>
2022-07-29 13:01:38 -0600

    New switch classifier
    Should work for n objects now.

--- bci_essentials/classification.py
+++ bci_essentials/classification.py
@@ -751,8 +751,142 @@ class mi_classifier(generic_classifier):
 
         return pred
 
+class mi_classifier(generic_classifier):
+    def set_mi_classifier_settings(self, n_splits=3, type="TS", pred_threshold=0.5, random_seed = 42):
+        # Build the cross-validation split
+        self.n_splits = n_splits
+        self.cv = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=random_seed)
+
+        # Initialize the classifier type
+        ts = TSclassifier()
+        mdm = MDM(metric=dict(mean='riemann', distance='riemann'))
+        lr = LogisticRegression()
+        #csp = CSP()
+
+        # Tangent Space Logistic Regression
+        if type == "TS":
+            ts = TSclassifier()
+            self.clf_model = Pipeline([("Tangent Space", ts)])
+            self.clf = Pipeline([("Tangent Space", ts)])
+
+        # Minimum Distance to Mean 
+        elif type == "MDM":
+            self.clf_model = Pipeline([("MDM", mdm)])
+            self.clf = Pipeline([("MDM", mdm)])
+
+        # CSP + Logistic Regression (REQUIRES MNE CSP)
+        # elif type == "CSP-LR":
+        #     self.clf_model = Pipeline([('CSP', csp), ('LogisticRegression', lr)])
+        #     self.clf = Pipeline([('CSP', csp), ('LogisticRegression', lr)])
+
+        else:
+            print("Classifier type not defined") 
+
+        # Threshold
+        self.pred_threshold = pred_threshold
+
+        # Rebuild from scratch with each training
+        self.rebuild = True
+
+
+
+    def fit(self):
+        # get dimensions
+        nwindows, nchannels, nsamples = self.X.shape 
+
+        # do the rest of the training if train_free is false
+        self.X = np.array(self.X)
+
+        # Try rebuilding the classifier each time
+        if self.rebuild == True:
+            self.next_fit_window = 0
+            self.clf = self.clf_model
+
+        # get channel subset
+        #self.get_subset()
+
+        # get temporal subset
+        subX = self.X[self.next_fit_window:,:,:]
+        suby = self.y[self.next_fit_window:]
+        self.next_fit_window = nwindows
+
+        # Init predictions to all false 
+        preds = np.zeros(nwindows)
+
+        
+        for train_idx, test_idx in self.cv.split(subX,suby):
+            X_train, X_test = subX[train_idx], subX[test_idx]
+            y_train, y_test = suby[train_idx], suby[test_idx]
+
+            # get the covariance matrices for the training set
+            X_train_cov = Covariances().transform(X_train)
+            X_test_cov = Covariances().transform(X_test)
+
+            # fit the classsifier
+            self.clf.fit(X_train_cov, y_train)
+            preds[test_idx] = self.clf.predict(X_test_cov)
+
+        # Print performance stats
+        # accuracy
+        correct = preds == self.y
+        #print(correct)
+
+        self.offline_window_count = nwindows
+        self.offline_window_counts.append(self.offline_window_count)
+
+        # accuracy
+        accuracy = sum(preds == self.y)/len(preds)
+        self.offline_accuracy.append(accuracy)
+        print("accuracy = {}".format(accuracy))
+
+        # precision
+        precision = precision_score(self.y,preds)
+        self.offline_precision.append(precision)
+        print("precision = {}".format(precision))
+
+        # recall
+        recall = recall_score(self.y, preds)
+        self.offline_recall.append(recall)
+        print("recall = {}".format(recall))
+
+        # confusion matrix in command line
+        cm = confusion_matrix(self.y, preds)
+        self.offline_cm = cm
+        print("confusion matrix")
+        print(cm)
+
+    def predict(self, X):
+        # if X is 2D, make it 3D with one as first dimension
+        if len(X.shape) < 3:
+            X = X[np.newaxis, ...]
+
+        X = self.get_subset(X)
+
+        # Troubleshooting
+        #X = self.X[-6:,:,:]
+
+        print("the shape of X is", X.shape)
+
+        X_cov = Covariances().transform(X)
+        #X_cov = X_cov[0,:,:]
+
+        pred = self.clf.predict(X_cov)
+        pred_proba = self.clf.predict_proba(X_cov)
+
+        for i in range(len(pred)):
+            self.predictions.append(pred[i])
+            self.pred_probas.append(pred_proba[i])
+
+        # add a threhold
+        #pred = (pred_proba[:] >= self.pred_threshold).astype(int) # set threshold as 0.3
+
+        return pred
+
 class switch_classifier(generic_classifier):
     def set_switch_classifier_settings(self, n_splits = 2, rebuild = True, random_seed = 42, activation_main = 'relu', activation_class = 'sigmoid'):
+
+        self.activation_main = activation_main
+        self.activation_class = activation_class
 
         self.n_splits = n_splits
         self.cv = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=random_seed)
@@ -763,22 +897,20 @@ class switch_classifier(generic_classifier):
         )
 
         # CHANGE THE CLASSIFIER HERE IF YOU WANT
-        self.clf0and1 = Sequential([
+        self.clf = Sequential([
             Flatten(),
-            Dense(units=8, input_shape=(4,), activation=activation_main),
-            Dense(units=16, activation=activation_main),
-            Dense(units=3, activation=activation_class)
+            Dense(units=8, input_shape=(4,), activation=self.activation_main),
+            Dense(units=16, activation=self.activation_main)
         ])
 
-        self.clf0and2 = Sequential([
+        self.clf_model = Sequential([
             Flatten(),
-            Dense(units=8, input_shape=(4,), activation=activation_main),
-            Dense(units=16, activation=activation_main),
-            Dense(units=3, activation=activation_class)
+            Dense(units=8, input_shape=(4,), activation=self.activation_main),
+            Dense(units=16, activation=self.activation_main)
         ])
 
-        # self.clf0and1 = MDM()
-
+        self.clfs = []
+        self.weights = []
 
     def fit(self):
         # get dimensions
@@ -792,8 +924,9 @@ class switch_classifier(generic_classifier):
         self.num_classifiers = len(list(np.unique(self.y))) - 1
         print(f"Number of classes: {self.num_classifiers}")
 
-        # make a list to hold all of the classifiers
-        self.clfs = []
+        if len(self.clf.layers) == 3:
+            self.clf.add(Dense(units=self.num_classifiers+1, activation=self.activation_class))
+            self.clf_model.add(Dense(units=self.num_classifiers+1, activation=self.activation_class))
 
         # loop through and build the classifiers
         for i in range(self.num_classifiers):
@@ -806,7 +939,6 @@ class switch_classifier(generic_classifier):
             # Try rebuilding the classifier each time
             if self.rebuild == True:
                 self.next_fit_window = 0
-                # tf.keras.backend.clear_session()
 
             subX = X_class[self.next_fit_window:,:,:]
             suby = y_class[self.next_fit_window:]
@@ -821,52 +953,22 @@ class switch_classifier(generic_classifier):
                 scaler_train = preprocessing.StandardScaler().fit(X_train)
                 X_train_scaled = scaler_train.transform(X_train)
 
-                print(f"The shape of X_train_scaled is {X_train_scaled.shape}")
-
                 z_dim, y_dim, x_dim = X_test.shape
                 X_test = X_test.reshape(z_dim, x_dim*y_dim)
                 scaler_test = preprocessing.StandardScaler().fit(X_test)
                 X_test_scaled = scaler_test.transform(X_test)
 
-                if i == 0:
-                    # Compile the model
-                    print("\nWorking on first model...")
-                    self.clf0and1.compile(optimizer=Adam(learning_rate=0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-                    # Fit the model
-                    self.clf0and1.fit(x=X_train_scaled, y=y_train, batch_size=5, epochs=4, shuffle=True, verbose=2, validation_data=(X_test_scaled, y_test)) # Need to reshape X_train
-                    self.weights1 = self.clf0and1.get_weights()
-                else:
-                    print("\nWorking on second model...")
-                    # Compile the model
-                    self.clf0and2.compile(optimizer=Adam(learning_rate=0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-                    # Fit the model
-                    self.clf0and2.fit(x=X_train_scaled, y=y_train, batch_size=5, epochs=4, shuffle=True, verbose=2, validation_data=(X_test_scaled, y_test)) # Need to reshape X_train
-                    self.weights2 = self.clf0and2.get_weights()
+                # Compile the model
+                self.clf.compile(optimizer=Adam(learning_rate=0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+                # Fit the model
+                self.clf.fit(x=X_train_scaled, y=y_train, batch_size=5, epochs=4, shuffle=True, verbose=2, validation_data=(X_test_scaled, y_test)) # Need to reshape X_train
+            
+            self.weights = self.clf.get_weights()
 
-            # Print performance stats
-            # accuracy
-            # correct = preds == self.y
-            # #print(correct)
+            self.clfs.append(self.clf)
+            self.weights.append(self.weights)
 
-            '''self.offline_window_count = nwindows
-            self.offline_window_counts.append(self.offline_window_count)
-            # accuracy
-            accuracy = sum(preds == self.y)/len(preds)
-            self.offline_accuracy.append(accuracy)
-            print("accuracy = {}".format(accuracy))
-            # precision
-            precision = precision_score(self.y,preds)
-            self.offline_precision.append(precision)
-            print("precision = {}".format(precision))
-            # recall
-            recall = recall_score(self.y, preds)
-            self.offline_recall.append(recall)
-            print("recall = {}".format(recall))
-            # confusion matrix in command line
-            cm = confusion_matrix(self.y, preds)
-            self.offline_cm = cm
-            print("confusion matrix")
-            print(cm)'''
+            self.clf = self.clf_model
 
     def predict(self, X):
         # if X is 2D, make it 3D with one as first dimension
@@ -875,42 +977,25 @@ class switch_classifier(generic_classifier):
 
         print("the shape of X is", X.shape)
 
-        self.predict0and1 = Sequential([
-            Flatten(),
-            Dense(units=8, input_shape=(4,), activation='relu'),
-            Dense(units=16, activation='relu'),
-            Dense(units=3, activation='sigmoid')
-        ])
+        self.pred_clfs = []
 
-        self.predict0and2 = Sequential([
-            Flatten(),
-            Dense(units=8, input_shape=(4,), activation='relu'),
-            Dense(units=16, activation='relu'),
-            Dense(units=3, activation='sigmoid')
-        ])
-
+        # Reshaping data
         z_dim, y_dim, x_dim = X.shape
         X_predict = X.reshape(z_dim, x_dim*y_dim)
         scaler_train = preprocessing.StandardScaler().fit(X_predict)
         X_predict_scaled = scaler_train.transform(X_predict)
 
-        pred0and1 = self.predict0and1.predict(X_predict_scaled)
-        pred0and2 = self.predict0and2.predict(X_predict_scaled)
-
         final_predictions = np.array([])
 
-        for row1, row2 in zip(pred0and1, pred0and2):
-            if row1[0] > row1[1] and row2[0] > row2[2]:
-                final_predictions = np.append(0, final_predictions)
-            elif row1[0] > row1[1] and row2[0] < row2[2]:
-                final_predictions = np.append(2, final_predictions)
-            elif row1[0] < row1[1] and row2[0] > row2[2]:
-                final_predictions = np.append(1, final_predictions)
-            elif row1[0] < row1[1] and row2[0] < row2[2]:
-                if row1[1] > row2[2]:
-                    final_predictions = np.append(1, final_predictions)
-                else:
-                    final_predictions = np.append(2, final_predictions)
+        # Making predictions
+        for i in range(len(self.clfs) - 1):
+            print(f"In the for loop. i value = {i}")
+            preds = self.clfs[i].predict(X_predict_scaled)
+            final_predictions = np.append(final_predictions, preds)
+
+        # Need to find out how to compare the values
+        final_predictions[final_predictions > 0.5] = len(self.clfs)
+        final_predictions[final_predictions < 0.5] = 0
 
         return final_predictions
         
