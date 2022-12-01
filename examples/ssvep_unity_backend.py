2e8d80ce341c666f43d06f6394c29ed550e313d2
Brian Irvine <33841739+brianjohannes@users.noreply.github.com>
2022-12-01 11:55:50 -0700

    turned off channel selection

--- examples/ssvep_unity_backend.py
+++ examples/ssvep_unity_backend.py
@@ -26,11 +26,11 @@ test_ssvep.stream_online_eeg_data()
 
 test_ssvep.classifier.set_ssvep_settings(n_splits=5, random_seed=42, n_harmonics=3, f_width=0.5, covariance_estimator="oas")
 
-# Channel Selection
-initial_subset=[]
-test_ssvep.classifier.setup_channel_selection(method = "SBFS", metric="accuracy", initial_channels = initial_subset,    # wrapper setup
-                                max_time= 999, min_channels=2, max_channels=14, performance_delta=0,      # stopping criterion
-                                n_jobs=-1, print_output="verbose") 
+# # Channel Selection
+# initial_subset=[]
+# test_ssvep.classifier.setup_channel_selection(method = "SBFS", metric="accuracy", initial_channels = initial_subset,    # wrapper setup
+#                                 max_time= 999, min_channels=2, max_channels=14, performance_delta=0,      # stopping criterion
+#                                 n_jobs=-1, print_output="verbose") 
 
 test_ssvep.main(online=True, training=True, max_samples=5120, pp_type="bandpass", pp_low=3, pp_high=50)
 
