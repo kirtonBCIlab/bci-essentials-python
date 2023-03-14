67f834fab9da0faf36f1ac905080a6f8346ede84
brianjohannes <33841739+brianjohannes@users.noreply.github.com>
2023-03-14 10:22:55 -0600

    Updates to get_bandpower_features
    - replaced scipy. trapz with np.trapz
    - improved documentation
    - removed norm bandpower

--- bci_essentials/resting_state.py
+++ bci_essentials/resting_state.py
@@ -61,12 +61,12 @@ def bandpower(data, fs, fmin, fmax, normalization=None):
     if normalization == "sum":
         Pxx = Pxx / Pxx.sum(axis=1).reshape((Pxx.shape[0], 1))
 
-    ind_local_min = scipy.argmax(f > fmin) - 1
-    ind_local_max = scipy.argmax(f > fmax) - 1
+    ind_local_min = np.argmax(f > fmin) - 1
+    ind_local_max = np.argmax(f > fmax) - 1
 
     power = np.zeros([nchannels])
     for i in range(nchannels):
-        power[i] = scipy.trapz(Pxx[i, ind_local_min:ind_local_max], f[ind_local_min:ind_local_max])
+        power[i] = np.trapz(Pxx[i, ind_local_min:ind_local_max], f[ind_local_min:ind_local_max])
 
     return power
 
@@ -177,10 +177,12 @@ def get_bandpower_features(data, fs, transition_freqs=[0, 4, 8, 12, 30]):
         a np array of the absolute bandpower of provided bands, last 
         item is the total bandpower, length is equal to length of 
         transition_freqs
-    norm_bandpower : array_like
-        a np array of the normalized bandpower of provided bands, last 
-        item is the total bandpower, length is equal to length of 
-        transition_freqs
+    rel_bandpower : array_like
+        a np array of the relative bandpower of provided bands with
+        respect to the entire region of interest from transition_freqs[0]
+        to transition_freqs[-1], last item is the total relative 
+        bandpower and should always equal 1, length is equal to length 
+        of transition_freqs
     rel_bandpower_mat : array_like
         a np array of the relative bandpower of provided bands such
         location (R,C) is corresponds to the power of R relative to C,
@@ -192,7 +194,6 @@ def get_bandpower_features(data, fs, transition_freqs=[0, 4, 8, 12, 30]):
 
     # Initialize
     abs_bandpower = np.zeros((len(transition_freqs), W))
-    norm_bandpower = np.zeros((len(transition_freqs), W))
     rel_bandpower = np.zeros((len(transition_freqs), W))
     rel_bandpower_mat = np.zeros((len(transition_freqs), len(transition_freqs), W))
 
@@ -213,44 +214,27 @@ def get_bandpower_features(data, fs, transition_freqs=[0, 4, 8, 12, 30]):
             # The last item is the total
             if tf == len(transition_freqs) - 1:
                 abs_bandpower[tf, win] = np.sum(abs_bandpower[:tf, win])
-                norm_bandpower[tf, win] = np.sum(norm_bandpower[:tf, win])
                 continue
 
             fmin = transition_freqs[tf]
             fmax = transition_freqs[tf + 1]
 
-            # # Normalize by sum
-            # norm_Pxx = np.zeros(Pxx.shape)
-            # whole_Pxx np.zeros(Pxx.shape)
-            # for c in range(C):
-            #     norm_Pxx[c,:] = Pxx[c,:] / Pxx[c,:].sum()
-            #     whole_Pxx[c,:]
-            #     # norm_Pxx[c,:] = Pxx[c,:] / Pxx[c,:].sum(axis=1).reshape((Pxx.shape[0], 1))
-
-            ind_local_min = scipy.argmax(f > fmin) - 1
-            ind_local_max = scipy.argmax(f > fmax) - 1
+            ind_local_min = np.argmax(f > fmin) - 1
+            ind_local_max = np.argmax(f > fmax) - 1
 
             # Get power for each channel
             abs_power = np.zeros([C])
             norm_power = np.zeros([C])
             for ch in range(C):
-                abs_power[ch] = scipy.trapz(
+                abs_power[ch] = np.trapz(
                     Pxx[ch, ind_local_min:ind_local_max], f[ind_local_min:ind_local_max]
                 )
-                # norm_power[ch] = scipy.trapz(
-                #     norm_Pxx[ch, ind_local_min:ind_local_max], f[ind_local_min:ind_local_max]
-                # )
-                # norm_power[ch] = abs_power[ch] / scipy.trapz(
-                #     Pxx[ch, ind_global_min:ind_global_max], f[ind_global_min:ind_global_max]
-                # )
 
             # Median across all channels
             abs_bandpower[tf, win] = np.median(abs_power)
 
 
         rel_bandpower[:, win] = abs_bandpower[:, win] / abs_bandpower[-1, win]
-
-
 
         # Calculate the relative power of each band
         for tf1 in range(len(transition_freqs)):
