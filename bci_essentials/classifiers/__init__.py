fcec57f226a49036354bf82476e718fd0ce48fe7
Daniel Comaduran <dcomadur@ucalgary.ca>
2023-07-20 13:11:12 -0600

    Finished with SSVEP classifiers

--- bci_essentials/classifiers/__init__.py
+++ bci_essentials/classifiers/__init__.py
@@ -7,3 +7,5 @@
 from .generic_classifier import Generic_classifier
 from .erp_rg_classifier import ERP_rg_classifier
 from .mi_classifier import MI_classifier
+from .ssvep_riemmanian_mdm_classifier import SSVEP_riemannian_mdm_classifier
+from .ssvep_basic_tf_classifier import SSVEP_basic_tf_classifier
