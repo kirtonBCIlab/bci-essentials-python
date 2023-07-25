5eb42eff85824cbe708f9db341743e3a0276df94
Brian Irvine <33841739+brianjohannes@users.noreply.github.com>
2023-07-25 09:43:52 -0600

    Removed comments

--- bci_essentials/classification/__init__.py
+++ bci_essentials/classification/__init__.py
@@ -12,12 +12,3 @@ from .ssvep_basic_tf_classifier import SSVEP_basic_tf_classifier
 from .null_classifier import Null_classifier
 from .switch_mdm_classifier import Switch_mdm_classifier
 from .switch_deep_classifier import Switch_deep_classifier
-
-# from . import generic_classifier
-# from . import erp_rg_classifier
-# from . import mi_classifier
-# from . import ssvep_riemannian_mdm_classifier
-# from . import ssvep_basic_tf_classifier
-# from . import null_classifier
-# from . import switch_mdm_classifier
-# from . import switch_deep_classifier
