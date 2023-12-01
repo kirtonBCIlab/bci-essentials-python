bd4ea733abe5613a8c4e7b38a1921a3fba9a74d4
Greg Wilding <gregory.wilding@ucalgary.ca>
2023-12-01 11:07:48 -0700

    Added LSL sources

--- bci_essentials/sources/lsl_sources.py
+++ bci_essentials/sources/lsl_sources.py
@@ -1,11 +1,68 @@
+from pylsl import StreamInlet, resolve_byprop
+
 from .sources import MarkerSource, EegSource
 
 
-class LslEegSource(EegSource):
-    def __init__(self):
-        print("hello")
-
-
 class LslMarkerSource(MarkerSource):
-    def __init__(self):
-        print("hello")
+    def __init__(self, timeout: float = 5.0):
+        marker_stream = resolve_byprop("type", "LSL_Marker_Strings", timeout=timeout)
+        self.__inlet = StreamInlet(marker_stream[0], processing_flags=0)
+        self.__info = self.__inlet.info()
+
+    @property
+    def name(self) -> str:
+        return self.__info.name()
+
+    def get_markers(self) -> tuple[list[list] | None, list]:
+        samples, timestamps = self.__inlet.pull_chunk(timeout=0.1)
+        return [samples, timestamps]
+
+    def time_correction(self) -> float:
+        return self.__inlet.time_correction()
+
+
+class LslEegSource(EegSource):
+    def __init__(self, timeout: float = 5.0):
+        eeg_stream = resolve_byprop("type", "EEG", timeout=timeout)
+        self.__inlet = StreamInlet(eeg_stream[0], processing_flags=0)
+        self.__info = self.__inlet.info()
+
+    @property
+    def name(self) -> str:
+        return self.__info.name()
+
+    @property
+    def fsample(self) -> float:
+        return self.__info.nominal_srate()
+
+    @property
+    def nchannels(self) -> int:
+        return self.__info.channel_count()
+
+    @property
+    def channel_types(self) -> list[str]:
+        return self.__get_channel_properties("type")
+
+    @property
+    def channel_units(self) -> list[str]:
+        return self.__get_channel_properties("unit")
+
+    @property
+    def channel_labels(self) -> list[str]:
+        return self.__get_channel_properties("label")
+
+    def get_samples(self) -> tuple[list[list] | None, list]:
+        samples, timestamps = self.__inlet.pull_chunk(timeout=0.1)
+        return [samples, timestamps]
+
+    def time_correction(self) -> float:
+        return self.__inlet.time_correction()
+
+    def __get_channel_properties(self, property: str) -> list[str]:
+        properties = []
+        descriptions = self.__info.desc().child("channels").child("channel")
+        for i in range(self.nchannels):
+            value = descriptions.child_value(property)
+            properties.append(value)
+            descriptions = descriptions.next_sibling()
+        return properties
---
+++ tests/test_lsl_sources.py
@@ -0,0 +1,81 @@
+import unittest
+
+from pylsl import StreamInfo, StreamOutlet, IRREGULAR_RATE
+
+from bci_essentials.sources.lsl_sources import LslMarkerSource, LslEegSource
+
+
+class TestLslMarkerSource(unittest.TestCase):
+    def setUp(self) -> None:
+        self.sender = LslSender("LSL_Marker_Strings", 1)
+        self.source = LslMarkerSource()
+
+    def test_marker_name(self):
+        self.assertEqual(self.source.name, "LSL Test")
+
+    # def test_get_marker(self):
+    #     self.sender.mark()
+    #     samples, timestamps = self.source.get_markers()
+    #     self.assertIsNotNone(samples)
+    #     self.assertEqual(len(samples), 1)
+    #     self.assertEqual(len(timestamps), 1)
+
+    def test_marker_time_correction_is_probably_not_zero(self):
+        # this is a bit goofy, but a realy LSL stream probably won't have a correction of zero
+        self.assertNotEqual(self.source.time_correction(), 0.0)
+
+
+class TestLslEegSource(unittest.TestCase):
+    def setUp(self) -> None:
+        self.sender = LslSender("EEG", 8, 128.0)
+        self.source = LslEegSource()
+
+    def test_marker_name(self):
+        self.assertEqual(self.source.name, "LSL Test")
+
+    def test_eeg_fsample(self):
+        self.assertEqual(self.source.fsample, 128.0)
+
+    def test_eeg_nchannel(self):
+        self.assertEqual(self.source.nchannels, 8)
+
+    def test_eeg_channel(self):
+        self.assertEqual(len(self.source.channel_types), 8)
+        # hard to inject this, going to skip
+        # self.assertEqual(self.source.channel_types[0], "eeg")
+
+    def test_eeg_channel_units(self):
+        self.assertEqual(len(self.source.channel_units), 8)
+        # hard to inject this, going to skip
+        # self.assertEqual(self.source.channel_units[0], "microvolts")
+
+    def test_eeg_channel_labels(self):
+        self.assertEqual(len(self.source.channel_labels), 8)
+        # hard to inject this, going to skip
+        # self.assertEqual(self.source.channel_labels[0], "FP1")
+
+    # def test_get_eeg(self):
+    #     self.sender.mark()
+    #     samples, timestamps = self.source.get_markers()
+    #     self.assertIsNotNone(samples)
+    #     self.assertEqual(len(samples), 1)
+    #     self.assertEqual(len(timestamps), 1)
+
+    def test_eeg_time_correction_is_probably_not_zero(self):
+        # this is a bit goofy, but a realy LSL stream probably won't have a correction of zero
+        self.assertNotEqual(self.source.time_correction(), 0.0)
+
+
+class LslSender:
+    def __init__(self, streamtype: str, channels: int, rate: float = IRREGULAR_RATE):
+        info = StreamInfo(
+            "LSL Test", streamtype, channels, rate, channel_format="float32"
+        )
+        self.__outlet = StreamOutlet(info)
+        self.__mark_count: float = 0.0
+
+    # This won't work properly unless runnign in a separate thread / process
+    def mark(self):
+        marker = [self.__mark_count]
+        self.__outlet.push_sample(marker)
+        self.__mark_count += 1.0
