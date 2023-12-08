7c080486c89aad20447b7d0e7c8dfb4e0048a91f
Greg Wilding <gregory.wilding@ucalgary.ca>
2023-12-07 17:05:05 -0700

    Cleaning up path management for xdf source tests
    - tests run if executed from root of repo or inside tests/

--- tests/test_xdf_sources.py
+++ tests/test_xdf_sources.py
@@ -1,21 +1,21 @@
 import unittest
-import os
+import os.path
 from bci_essentials.sources.xdf_sources import XdfEegSource, XdfMarkerSource
 
 
 class TestXdfSourceNonexistentFiles(unittest.TestCase):
     def test_xdf_marker_source_raises_exception_on_bad_filename(self):
         with self.assertRaises(Exception):
-            XdfMarkerSource("./doesnt_exist.xdf")
+            XdfMarkerSource("doesnt_exist.xdf")
 
     def test_xdf_eeg_source_raises_exception_on_bad_filename(self):
         with self.assertRaises(Exception):
-            XdfEegSource("./doesnt_exist.xdf")
+            XdfEegSource("doesnt_exist.xdf")
 
 
 class TestXdfMarkerSource(unittest.TestCase):
     def setUp(self) -> None:
-        filepath = os.path.join("examples", "data", "rs_example.xdf")
+        filepath = os.path.join(os.path.dirname(__file__), "data", "test_data.xdf")
         self.source = XdfMarkerSource(filepath)
 
     def test_marker_name(self):
@@ -39,7 +39,7 @@ class TestXdfMarkerSource(unittest.TestCase):
 
 class TestXdfEegSource(unittest.TestCase):
     def setUp(self) -> None:
-        filepath = os.path.join("examples", "data", "rs_example.xdf")
+        filepath = os.path.join(os.path.dirname(__file__), "data", "test_data.xdf")
         self.source = XdfEegSource(filepath)
 
     def test_eeg_name(self):
