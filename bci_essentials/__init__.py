bf32ea8f5f98ce4e9de6acd7ee4e84979bbec206
daniellarbourque <daniellarosebourque@gmail.com>
2025-02-28 11:40:33 -0700

    Add deleted lines

--- bci_essentials/__init__.py
+++ bci_essentials/__init__.py
@@ -1,3 +1,10 @@
+# Import submodules to make them discoverable
+from . import classification
+from . import data_tank
+from . import io
+from . import paradigm
+from . import utils
+
 # Instantiate a parent logger for the library at default
 from .utils.logger import Logger
 
