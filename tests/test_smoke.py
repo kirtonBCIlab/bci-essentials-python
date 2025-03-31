15e36b46c9f557308dd14ce8f5209a20cad65b21
Daniel Comaduran <dcomadur@ucalgary.ca>
2025-03-31 15:31:59 -0600

    Add smoke test for new classifier

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -9,6 +9,9 @@ from bci_essentials.paradigm.ssvep_paradigm import SsvepParadigm
 from bci_essentials.data_tank.data_tank import DataTank
 from bci_essentials.classification.mi_classifier import MiClassifier
 from bci_essentials.classification.erp_rg_classifier import ErpRgClassifier
+from bci_essentials.classification.erp_rg_classifier_hyperparamgridsearch import (
+    ErpRgClassifierHyperparamGridSearch,
+)
 from bci_essentials.classification.ssvep_riemannian_mdm_classifier import (
     SsvepRiemannianMdmClassifier,
 )
@@ -96,6 +99,45 @@ class TestSmoke(unittest.TestCase):
         self.assertIsNotNone(classifier.predictions)
 
         logger.info("P300 test complete")
+
+    def test_p300_hyperparam_offline(self):
+        # Get the P300 example data from ./examples/data
+        xdf_path = os.path.join(os.path.dirname(__file__), "data", "p300_smoke.xdf")
+        eeg_source = XdfEegSource(xdf_path)
+        marker_source = XdfMarkerSource(xdf_path)
+
+        paradigm = P300Paradigm()
+        data_tank = DataTank()
+
+        # Select a classifier
+        classifier = ErpRgClassifierHyperparamGridSearch()
+        classifier.set_p300_clf_settings(
+            n_splits=5,
+            random_seed=42,
+        )
+
+        # Load the data
+        data = BciController(classifier, eeg_source, marker_source, paradigm, data_tank)
+
+        # Run main loop, this will do all of the classification for online or offline
+        data.setup(
+            online=False,
+        )
+        data.run()
+
+        # Check that a model was trained
+        self.assertIsNotNone(classifier.clf)
+
+        # Check that accuracy, precision, recall and covariance matrix exist
+        self.assertIsNotNone(classifier.offline_accuracy)
+        self.assertIsNotNone(classifier.offline_precision)
+        self.assertIsNotNone(classifier.offline_recall)
+        self.assertIsNotNone(classifier.offline_cm)
+
+        # Check that the list of class predictions exists
+        self.assertIsNotNone(classifier.predictions)
+
+        logger.info("P300 hyperparamer grid search test complete")
 
     def test_ssvep_offline(self):
         # Get the SSVEP example data from ./examples/data
